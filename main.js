b2ca94d6edf545bf4bee1d6217be4dcd8984071c
coolcoder700 <71060596+coolcoder700@users.noreply.github.com>
2020-11-07 16:59:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+var names=[];
+
+function submit()
+{
+    var name1= document.getElementById("student1").value;
+    var name2= document.getElementById("student2").value;
+    var name3= document.getElementById("student3").value;
+    var name4= document.getElementById("student4").value;
+    names.push(name1);
+    names.push(name2);
+    names.push(name3);
+    names.push(name4);
+    console.log(names);
+    document.getElementById("display_names").innerHTML=names;
+    document.getElementById("sortButton").style.display="inline";
+    document.getElementById("submitButton").style.display="none"; 
+    
+}
+function sorting()
+{
+    names.sort();
+    console.log(names);
+    document.getElementById("display_names").innerHTML=names;
+
+}
